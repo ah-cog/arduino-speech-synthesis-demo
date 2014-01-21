ea11bb0729225936a4dcb726fb69432c538d130d
Michael Gubbels <mrgubbels@gmail.com>
2014-01-21 03:29:06 -0500

    Cleaned up code.

--- Collider_Voice_Server/Collider_Voice_Server.ino
+++ Collider_Voice_Server/Collider_Voice_Server.ino
@@ -1,6 +1,7 @@
 /*
 
-Emic 2 Text-to-Speech Module: Basic Demonstration       
+Collider Voice
+(based on Emic 2 Text-to-Speech Module: Basic Demonstration)
                                                  
 Authors:
   Michael Gubbels [www.michaelgubbels.com]
@@ -8,9 +9,7 @@ Authors:
 
 Program Description:
 
-This program provides a simple demonstration of the Emic 2 Text-to-Speech
-Module. Please refer to the product manual for full details of system 
-functionality and capabilities.
+This program provides a basic voice server for Collider.
 
 */
 
@@ -29,8 +28,8 @@ boolean stringComplete = false;  // whether the string is complete
 
 boolean active = false;
 
-void setup()  // Set up code called once on start-up
-{
+void setup() { // Set up code called once on start-up
+
   Serial.begin(9600);
   
   // reserve 200 bytes for the inputString:
@@ -69,65 +68,29 @@ void loop() { // Main code, to run repeatedly
   }
 
   delay(500);    // 1/2 second delay
-    
-  // Sing a song
-//  singSong();
 
-  
-//  while(1) {      // Demonstration complete!
-  
-    // Read serial data
-//    if (Serial.available() > 0) {
-//      // read the incoming byte:
-//      incomingByte = (char) Serial.read();
-//      incomingStringLength++;
-//      
-//      // say what you got:
-//      Serial.print("I received: ");
-//      Serial.println(incomingByte);
-//      
-//      if (incomingByte == '\n') {
-//        Serial.println("END OF INPUT");
-//      }
-//    }
+//  singSong(); // Sing a song
 
-  // print the string when a newline arrives:
+  // Print the string when a newline arrives:
   if (stringComplete) {
-    Serial.println(inputString); 
-    
-    
-//    incomingStringLength = inputString.length();
-//    for (int i = 0; i < incomingStringLength; i++) {
-//      incomingString[i] = inputString.charAt(i);
-//    }
+    Serial.println(inputString);
     
     // Speak the text
     speakText(inputString);
-//    emicSerial.print('S');
-//    emicSerial.print(inputString);  // Send the desired string to convert to speech
-//    emicSerial.print('\n');
-//    digitalWrite(ledPin, HIGH);         // Turn on LED while Emic is outputting audio
-//    while (emicSerial.read() != ':');   // Wait here until the Emic 2 responds with a ":" indicating it's ready to accept the next command
-//    digitalWrite(ledPin, LOW);
     
-    // clear the string:
+    // Clear the string
     inputString = "";
     stringComplete = false;
   }
-  
-//    delay(500);
-//    digitalWrite(ledPin, HIGH);
-//    delay(500);              
-//    digitalWrite(ledPin, LOW);
-//  }
+
 }
 
-/*
-SerialEvent occurs whenever a new data comes in the
-hardware serial RX.  This routine is run between each
-time loop() runs, so using delay inside loop can delay
-response.  Multiple bytes of data may be available.
-*/
+/**
+ * SerialEvent occurs whenever a new data comes in the
+ * hardware serial RX.  This routine is run between each
+ * time loop() runs, so using delay inside loop can delay
+ * response.  Multiple bytes of data may be available.
+ */
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
